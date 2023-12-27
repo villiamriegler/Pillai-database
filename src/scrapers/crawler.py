b46188db3c87fac5eb3e55846637522294f2163f
Villiam riegler <ville@riegler.se>
2023-12-27 20:02:09 +0100

    Code refactor into functions and proper comments

--- src/scrapers/crawler.py
+++ src/scrapers/crawler.py
@@ -3,124 +3,167 @@ from bs4 import BeautifulSoup
 from scraper import *
 from pprint import pprint
 
-PAGES = {
-    "bipacksedel": 7,
-    "produktresume": 6,
-    "förpackningar": 30,
-    "fass-text": 3,
-    "bilder-och-delbarhet": 2000,
-    "miljöinformation": 78,
-    "skyddsinfo": 80
-}
+# viktig-patientinfo: https://www.fass.se/LIF/product?userType=2&nplId=20071201000025&docType=15&scrollPosition=0
 
-# 0 = No print
-# 1 = Print keys
-# 2 = Print all
-DEBUGING_PAGES = {
-    "bipacksedel": 1,
-    "produktresume": 1,
-    "förpackningar": 1,
-    "fass-text": 1,
-    "bilder-och-delbarhet": 1,
-    "miljöinformation": 1,
-    "skyddsinfo": 1
-}
 
-#viktig-patientinfo: https://www.fass.se/LIF/product?userType=2&nplId=20071201000025&docType=15&scrollPosition=0
-
-# Try to retrieva an URL
+# Fetches html content for web ardress and returns as bs4 soup
 def fetch_url(url):
+    # Send request for html content to webserver
     response = requests.get(url)
-    if response.status_code == 200:
+    if response.status_code == 200:     # If the request was successful
+        # Return html form page as bs4 soup
         return BeautifulSoup(response.content, 'html.parser')
-    else:
+    else:                               # Request was unsucessful, server responded with error or nothing
         raise ConnectionError('Could not connect to website')
 
 
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
-PRODUCT_BASE_LINK = "https://www.fass.se/LIF/product?nplId="
-
-
 # Print debuging info for all parsed data
+#   params: key(index of the page that was scraped, used to differenttiate between the information we want to display)
+#           result(the scraped content from the page)
 def debug(key, result):
+    # 0 = No print
+    # 1 = Print keys
+    # 2 = Print all
+    DEBUGING_PAGES = {
+        "bipacksedel": 1,
+        "produktresume": 1,
+        "förpackningar": 1,
+        "fass-text": 1,
+        "bilder-och-delbarhet": 1,
+        "miljöinformation": 1,
+        "skyddsinfo": 1
+    }
+
     match DEBUGING_PAGES[key]:
-        case 2:
+        case 2:     # Pretty print full dictionary
             pprint(result)
-        case 1:
+        case 1:     # Onl print the keys in the dictionary
             print(result.keys())
-        case _: 
+        case _:
             pass
 
 
-# Loop over every page in the a-ö list of pharmaceuticals
-for letter in ALPHABET:
-    # Get reference to current letter page
-    html_ref = f"https://www.fass.se/LIF/pharmaceuticallist?page={letter}"
-    soup = fetch_url(html_ref)
+# Scrapes the information from each page on a given url
+#       params: page(the page to scrape, used to differentiate diffrent scraping methods for diffrent pages) 
+#               soup(the html content of the page to scrape)
+def extract_page_information(page, soup):
+    result = {}
 
-    # Get all tags containing names of drugs
-    names = soup.select('.productResultPanel .expandcontent .innerlabel')
-    # Get all tags containing links to drug info
-    links = soup.select('.productResultPanel .expandcontent .linkList a')
+    # NOTE: some subcategories may be scraped using the same scraping method
+    # example of this is both the fass-text and miljöinformation.
+    # This is because the inforamtion followes the same format
+    match page:
+        case "bipacksedel":
+            # Keys retrived inside the medical-text
 
-    # Extract drug names
-    product_names = [name.get_text().strip() for name in names]
-    # Extract drug ids
-    product_ids = [link.get('href').strip()[-14:] for link in links]
+            # 'user-information', 'product-information', 'indication',
+            # 'caution-and-warnings', 'contraindication', 'caution',
+            # 'interaction', 'pregnancy', 'driving', 'substance-information',
+            # 'usage-and-administration', 'overdosage', 'missed',
+            # 'withdrawal', 'side-effects',
+            # 'additionalMonitoringInfo', 'storage', 'information-source',
+            # 'composition', 'appearance', 'prod-license'
+            result = extract_medical_text(soup)
+        case "produktresume":
+            # Keys retrived from first entry
 
-    count = 0
-    for product_id in product_ids:
-        count += 1
-        # Full link to product
-        full_url = PRODUCT_BASE_LINK + product_id
-        print(f"*****************************\n{full_url} {count}\n********************************")
+            # 'tradename', 'composition', 'product-form', 'clinical',
+            # 'pharmacological', 'pharmaceutical',
+            # 'prod-license', 'approval-number', 'approval-first-date', 'revision-date'
+            result = extract_product_resume(soup)
+        case "förpackningar":
+            # All the package information
+            result = extract_package_info(soup)
+        case "fass-text":
+            # Keys retrived inside the fass-text
 
-        # Check different pages for that product
-        for (page,number) in PAGES.items():
-            page_url = full_url + f"&docType={number}" 
-            soup = fetch_url(page_url)
-            result = {}
+            # 'indication', 'contraindication', 'dosage', 'caution',
+            # 'interaction', 'pregnancy', 'breastfeeding', 'fertility',
+            # driving', 'side-effects', 'overdosage', 'pharmacodynamic',
+            # 'pharmacokinetic', 'preclinical-info', 'composition', 'env-effect',
+            # 'handling-life-shelf-storage'
+            result = extract_fass_text(soup)
+        case "bilder-och-delbarhet":
+            result = extract_delbarhet(soup)
+        case "miljöinformation":
+            result = extract_fass_text(soup)
+        case "skyddsinfo":
+            result = extract_product_resume(soup)
 
-            if soup is None:
-                continue
+    debug(page, result)
+    return result
 
-            match page:
-                case "bipacksedel":
-                    # Keys retrived inside the medical-text
 
-                    # 'user-information', 'product-information', 'indication', 
-                    # 'caution-and-warnings', 'contraindication', 'caution', 
-                    # 'interaction', 'pregnancy', 'driving', 'substance-information', 
-                    # 'usage-and-administration', 'overdosage', 'missed', 
-                    # 'withdrawal', 'side-effects', 
-                    # 'additionalMonitoringInfo', 'storage', 'information-source', 
-                    # 'composition', 'appearance', 'prod-license'
-                    result = extract_medical_text(soup)
-                case "produktresume":
-                    # Keys retrived from first entry
-                    
-                    # 'tradename', 'composition', 'product-form', 'clinical', 
-                    # 'pharmacological', 'pharmaceutical', 
-                    # 'prod-license', 'approval-number', 'approval-first-date', 'revision-date'
-                    result = extract_product_resume(soup)
-                case "förpackningar": 
-                    # All the package information 
-                    result = extract_package_info(soup)
-                case "fass-text":
-                    # Keys retrived inside the fass-text
+# Walks all the pages for a given medecine and extracts the information
+#       params: full_url(the url of the base page to scrape)
+def crawl_pages(full_url):
+    # The url for each subcategory of medecine information can be indexed as
+    # the base_url with an appended docType selector. The docType selection
+    # uses the following magic numbers for each subcategory
+    PAGES = {
+        "bipacksedel": 7,
+        "produktresume": 6,
+        "förpackningar": 30,
+        "fass-text": 3,
+        "bilder-och-delbarhet": 2000,
+        "miljöinformation": 78,
+        "skyddsinfo": 80
+    }
 
-                    # 'indication', 'contraindication', 'dosage', 'caution', 
-                    # 'interaction', 'pregnancy', 'breastfeeding', 'fertility', 
-                    # driving', 'side-effects', 'overdosage', 'pharmacodynamic', 
-                    # 'pharmacokinetic', 'preclinical-info', 'composition', 'env-effect', 
-                    # 'handling-life-shelf-storage'
-                    result = extract_fass_text(soup)
-                case "bilder-och-delbarhet":
-                    result = extract_debarhet(soup)
-                case "miljöinformation":
-                    result = extract_fass_text(soup)
-                case "skyddsinfo":
-                    result = extract_product_resume(soup)
+    url_result = {}
+    # Iterate over the information subcategories
+    for (page, number) in PAGES.items():
+        # Creates the full url of subcategory to be scraped
+        page_url = full_url + f"&docType={number}"
 
-            debug(page,result)
-        
+        # Fetches html content of the page
+        soup = fetch_url(page_url)
+
+        # Check if page exists
+        if soup is None:
+            continue
+
+        # Extracting all information from medecine subcategory and stores the
+        # result by subcategory in a dictionary of dictionaries
+        url_result[page] = extract_page_information(page, soup)
+
+    return url_result
+
+
+# Crawls through all the medecines in the alpabetical list of medecines found on
+# https://www.fass.se/LIF/pharmaceuticalliststart?userType=2
+def crawl_alphabetical_list():
+    ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
+    PRODUCT_BASE_LINK = "https://www.fass.se/LIF/product?nplId="
+    # Loop over every page in the a-ö list of pharmaceuticals
+    for letter in ALPHABET:
+        # Get reference to current letter page
+        html_ref = f"https://www.fass.se/LIF/pharmaceuticallist?page={letter}"
+        soup = fetch_url(html_ref)
+
+        # Get all tags containing names of drugs
+        names = soup.select('.productResultPanel .expandcontent .innerlabel')
+        # Get all tags containing links to drug info
+        links = soup.select('.productResultPanel .expandcontent .linkList a')
+
+        # Extract drug names
+        product_names = [name.get_text().strip() for name in names]
+        # Extract drug ids
+        product_ids = [link.get('href').strip()[-14:] for link in links]    # The product link is relative 
+                                                                            # To get the absolute link we extract only the nplID of the link
+
+        count = 0
+        # Iterate over every product beginning with current letter
+        for product_id in product_ids:
+            count += 1
+            # Full link to product
+            full_url = PRODUCT_BASE_LINK + product_id
+            print(
+                f"*****************************\n{full_url} {count}\n********************************")
+
+            # Retrive all information from pages
+            page_information = crawl_pages(full_url)
+
+
+if __name__ == '__main__':
+    crawl_alphabetical_list()
--- src/scrapers/scraper.py
+++ src/scrapers/scraper.py
@@ -1,12 +1,12 @@
 
 PRODUCT_ID_INDEX = 5
 
-"""
-    Bipacksedel extraction
-"""
+
+# Scarping method for extracting the bipacksedel subcategory on fass.se
+# Example page: https://www.fass.se/LIF/product?userType=2&nplId=20190822000136&docType=7&scrollPosition=352
 def extract_medical_text(soup):
     data = {}
-    # Select full div containing the medical text 
+    # Select full div containing the medical text
     fass_content = soup.select('.fass-content')
 
     # Every section is labled with an a tag defining what information followes
@@ -18,37 +18,43 @@ def extract_medical_text(soup):
             title = section.get('id')
         except:
             continue
-        headerpart_div = fass_content[0].find(id=title).find_next('div').get_text()    # Extract all text content folowing the lable
-        info = " ".join(headerpart_div.split())                             # Clean up text removing \n \t and whitespace 
+
+        headerpart_div = fass_content[0].find(id=title).find_next('div').get_text()     # Extract all text content folowing the lable
+        info = " ".join(headerpart_div.split())                                         # Clean up text removing \n \t and whitespace
         data[title] = data.get(title, "") + info
 
     return data
 
+
+# Scraping method to retrive information from the fass-text subcategory
+# Example page: https://www.fass.se/LIF/product?userType=2&nplId=20190822000136&docType=3&scrollPosition=352
 def extract_fass_text(soup):
     data = {}
-    # Select full div containing the medical text 
+    # Select full div containing the medical text
     fass_content = soup.select('.fass-content')
 
     # Every section is labled with an a tag defining what information followes
     #   used as index for our data dictionary
     headers = fass_content[0].find_all('h2')
-
 
     for section in headers:
         try:
             title = section.find('a', id=True).get('id')
         except:
             continue
-        
-        headerpart_div = fass_content[0].find(id=title).find_next('div').get_text()    # Extract all text content folowing the lable
-        info = " ".join(headerpart_div.split())                             # Clean up text removing \n \t and whitespace 
+
+        headerpart_div = fass_content[0].find(id=title).find_next('div').get_text()     # Extract all text content folowing the lable
+        info = " ".join(headerpart_div.split())                                         # Clean up text removing \n \t and whitespace
         data[title] = data.get(title, "") + info
 
     return data
 
+
+# Scraping method for the productresume page subcategory
+# Example page: https://www.fass.se/LIF/product?userType=2&nplId=20190822000136&docType=6&scrollPosition=820
 def extract_product_resume(soup):
     data = {}
-    # Select full div containing the medical text 
+    # Select full div containing the medical text
     fass_content = soup.select('.fass-content')
 
     # Every section is labled with an a tag defining what information followes
@@ -57,11 +63,16 @@ def extract_product_resume(soup):
 
 
     for section in headers:
+        # Some sections do not follow the standard even though they exist
+        # as if fass.se uses hand written html. Therefor we still extract
+        # the information but cannot differatiate the category
         try:
             title = section.find('a', id=True).get('id')
         except:
             title = 'övrig-information'
 
+        # This page does not group the information in a div
+        # therefor we extract all the information between two headings
         html = ""
         for tag in section.next_siblings:
             if tag.name == "h2":
@@ -69,28 +80,33 @@ def extract_product_resume(soup):
             else:
                 html += str(tag.get_text())
 
-        info = " ".join(html.split()) # Clean up text removing \n \t and whitespace 
+        info = " ".join(html.split())           # Clean up text removing \n \t and whitespace 
         data[title] = data.get(title, "") + info
-    
     return data
 
-def extract_debarhet(soup):
+
+# Extracts infromation about how the pill can be consumed or split
+# Example page: https://www.fass.se/LIF/product?userType=2&nplId=20190822000136&docType=2000&scrollPosition=820
+def extract_delbarhet(soup):
     div = soup.select(".tablet-delbarhet-information ")
     if len(div) == 0:
         return {}
 
-    result = " ".join(div[0].get_text().split())
-    colon_index = result.find(':') 
+    result = " ".join(div[0].get_text().split())    # Text cleanup
+    colon_index = result.find(':')                  # Information format "Delbarhetsinformation: ..."
+                                                    # this finds the index of the colon
 
     return {"delbarhets-information": result[colon_index + 2:]} if colon_index > 0 else {}
 
+
+# Extracts the product id from diffrent packages of the same medecine
+# Example page: https://www.fass.se/LIF/product?userType=2&nplId=20190822000136&docType=30&scrollPosition=850
 def extract_package_info(soup):
     # List to keep all found product ID:s
     productIDs = []
 
     # Find all tables in fass-content
     tables = soup.select('.fass-content table')
-    
     # Go through all tables
     for table in tables:
         # Get all rows in each table
@@ -102,6 +118,4 @@ def extract_package_info(soup):
             # Only extract the product ID
             productIDs.append(cells[PRODUCT_ID_INDEX].get_text().strip())
 
-    return {"product-id": productIDs}          
-
-
+    return {"product-id": productIDs}
