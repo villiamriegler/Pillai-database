2ebec243ee9e47ed3350f62647803f11cf688a8e
Villiam riegler <ville@riegler.se>
2023-12-28 14:02:43 +0100

    typo fix

--- src/scrapers/test.py
+++ src/scrapers/test.py
@@ -7,7 +7,7 @@ import timeit
 def pref_scrapers():
     AVERAGE_ITERATIONS = 50     # Defines the number of iterations to average the result over
 
-    print(f"Mesuaring avreage runtime over {AVERAGE_ITERATIONS} iterations for all scrapers\n----------------------")
+    print(f"Measuring avreage runtime over {AVERAGE_ITERATIONS} iterations for all scrapers\n----------------------")
     print("Medical text:\t\t", timeit.timeit(
         'extract_medical_text(soup)',       # The code to test
         'soup = fetch_url("https://www.fass.se/LIF/product?userType=2&nplId=20190822000136&docType=7&scrollPosition=352")',     # Any necessary setup
