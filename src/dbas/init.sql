9a5e5d226f7ec84eebd1c2c70e89ec2a5265d76b
ALexander Runebou <alerun@kth.se>
2024-01-10 12:29:55 +0100

    feat: change database to include products and leaflets

--- src/dbas/init.sql
+++ src/dbas/init.sql
@@ -1,6 +1,28 @@
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+
+CREATE TYPE LANGUAGE AS ENUM ('SV', 'EN');
+
+CREATE TABLE IF NOT EXISTS leaflets (
+    leaflet_id INT PRIMARY KEY,
+    language LANGUAGE,
+    text TEXT
 );
 
-INSERT INTO products (name) VALUES ('Initial Product');
+CREATE TABLE IF NOT EXISTS products (
+    ean VARCHAR(14) PRIMARY KEY,
+    npl VARCHAR(14) NOT NULL UNIQUE,
+    leaflet_id INT REFERENCES LEAFLETS,
+    name VARCHAR(64) 
+);
+
+
+
+
+INSERT INTO leaflets (leaflet_id, language, text) VALUES (1, 'SV', 'X');
+INSERT INTO leaflets (leaflet_id, language, text) VALUES (2, 'SV', 'Y');
+
+
+INSERT INTO products (ean, npl, name, leaflet_id) VALUES ('07046260070127', '20010601000082', 'Alvedon®', 1);
+INSERT INTO products (ean, npl, name, leaflet_id) VALUES ('07350099990328', '20170809000076', 'Magnesium EQL Pharma', 2);
+
+
+
