c430aa38bab0cf4f2e56e3c8d21802850f8b34e4
etisell <etisell@kth.se>
2024-01-30 21:27:01 +0100

    feat: Add script to insert EAN-codes from JSON to sql

--- src/dbas/inserteans.py
+++ src/dbas/inserteans.py
@@ -1,13 +1,55 @@
 import json
+import os
+import psycopg2
 
-f = open('ean.json')
+JSON_FILE = 'ean.json'
 
-data = json.load(f)
+# Load environment variables
+db_name = os.getenv('POSTGRES_DB')
+db_user = os.getenv('POSTGRES_USER')
+db_pass = os.getenv('POSTGRES_PASSWORD')
+db_host = 'db'  # Docker Compose service name for the DB
+
+# Insert data into database
+def insert_into_database():
+    # Connection to the PostgreSQL database
+    conn = psycopg2.connect(
+        dbname=db_name, 
+        user=db_user, 
+        password=db_pass, 
+        host=db_host
+    )
+
+    # Establish cursor
+    cursor = conn.cursor()
+
+    try:
+        # Open JSON file
+        f = open(JSON_FILE)
+
+        # Load data from JSON file
+        data = json.load(f)
+
+        # Loop through all EAN-codes in JSON file
+        for ean in data:
+            nplID = data[ean]
+            
+            # Insert EAN-codes and nplIDs
+            query = "INSERT INTO eans (ean, nplID) VALUES (%s, %s) ON CONFLICT (ean) DO NOTHING"                  # TODO ändra så att det är rätt table med rätt attribut
+            cursor.execute(query, (ean, nplID))
+
+        conn.commit()
+    except psycopg2.Error as e:
+        # Handle errors
+        print(f"Database error: {e}")
+        conn.rollback()
+    finally:
+        # Close connection
+        cursor.close()
+        conn.close()
+        f.close()
 
 
 
-f.close()
-
-
-def main():
-    print("Hejhej")
+if __name__ == '__main__':
+    insert_into_database()
