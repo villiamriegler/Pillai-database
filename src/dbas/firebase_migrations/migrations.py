e6c4bd493e08e4f16343583a4751eeb70efda0b3
ALexander Runebou <alerun@kth.se>
2024-02-18 23:50:55 +0100

    fix: convert keys using special characters to camelCase for FlutterFlow compability

--- src/dbas/firebase_migrations/migrations.py
+++ src/dbas/firebase_migrations/migrations.py
@@ -3,12 +3,13 @@ from os import path, walk, getenv
 from firebase_admin import initialize_app, firestore, credentials
 from dotenv import load_dotenv
 from json import load, loads
-
+import re
 
 load_dotenv()
 
 NPL_LENGHT = 14
 LEAFLET_DIR = "../../scrapers/data/products"
+SPECIAL_CHARS = ['-', '_']
 
 # Connecting to Firebase
 cert = loads(getenv("GOOGLE_APPLICATION_CREDENTIALS"))
@@ -20,6 +21,31 @@ leaflets = firestore.client().collection('leaflets')
 
 (_, _, filenames) = next(walk(LEAFLET_DIR))  # Get all Files in product directory
 
+# Convert string to camelCase
+def str_to_camel_case(s):
+    # Ignore non-strings
+    if not isinstance(s, (str, str)):
+        raise ValueError('Input must be a string')
+    
+    # Split at any special characters
+    words = re.split('[^a-zA-Z0-9åäöÅÄÖ]+', s)  
+    
+    # Merge into camelCase
+    cc_str = words[0] + ''.join(word.capitalize() for word in words[1:])
+    return cc_str
+
+# Convert dictionary keys to camelCase
+def keys_to_camel_case(obj):
+    if isinstance(obj, dict):
+        # Recursive call for dicts
+        return {str_to_camel_case(k): keys_to_camel_case(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        # All elements in lists
+        return [keys_to_camel_case(e) for e in obj]
+    else: 
+        # Standard string values
+        return obj
+
 pbar = tqdm(filenames)  # Progress bar
 for file in pbar:
     npl = file[:NPL_LENGHT]  # Get nplID from filename
@@ -29,6 +55,11 @@ for file in pbar:
     with open(path.join(LEAFLET_DIR, file)) as json_file:
         data = load(json_file)
 
-    post_process = {key: {k: v for k, v in val.items() if v} for key, val in data.items() if val}  # Remove any empty records
+    # Remove any empty records
+    post_process = {key: {k: v for k, v in val.items() if v} for key, val in data.items() if val}  
 
-    leaflets.document(npl).set(post_process)
+    # Convert to camelCase
+    cc_dict = keys_to_camel_case(post_process)
+    
+
+    leaflets.document(npl).set(cc_dict)
